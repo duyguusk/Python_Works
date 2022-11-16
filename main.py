a8034c0a7b4a293135fc1dc733b69f17cc823406
Duygu Selin Kara <97129378+duyguusk@users.noreply.github.com>
2022-11-16 21:12:33 +0100

    Add files via upload
    firs commit

---
+++ main.py
@@ -0,0 +1,33 @@
+print("merhaba Etiya")
+#!string
+text="10"
+print(text)
+
+#string  INTEGER
+number=10
+dnumber=8.3
+
+print(number+5)
+print(number/5)
+print(number%5)
+
+#string
+print(1==2)
+print(1!=2)
+print(2!=2)
+print(2>=2)
+print(10/2==0)
+print(10%2==0)
+print(50/2==25)
+print(50/2==25.0)
+print(50%2==0)
+
+#STRING OPERASYONLARI
+text="Merhaba Etiya"
+print(text.upper())
+print(text.lower())
+
+name= "Duygu"
+age= 23
+company= "Etiya"
+print(f"{name} {age} yasinda {company} ' da calisiyor")
---
+++ odev1.py
@@ -0,0 +1,24 @@
+haber="HABERİNİZ OLSUN" #string
+vade=12 #integer
+faizOrani1=1.47 #float
+faizorani2=1.44 
+
+print(haber)
+print(type(haber))
+print(type(vade))
+print(type(faizOrani1))
+
+sayi1=10
+sayi2=20
+print(sayi1+sayi2)
+
+mesaj="Hoş geldiniz"
+musteriAdi="Meltem"
+musteriSoyadi="Kara"
+print(mesaj+musteriAdi+musteriSoyadi+"!")
+print(mesaj+" "+musteriAdi+" "+musteriSoyadi+"!")
+#f ile 
+print(f"{mesaj} {musteriAdi} {musteriSoyadi}! ")
+#degiskenle
+sonucmesaj= (mesaj+" "+musteriAdi+" "+musteriSoyadi+"!")
+print(sonucmesaj)
